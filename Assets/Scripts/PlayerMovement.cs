b040d087acb144467f0a5ddacd1dec6bc46a3631
wolfelias <w.e.kaminski@gmail.com>
2020-12-20 11:10:51 +0100

    initialized scene, added player quad and movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public int speed = 10;
+    private int cnt = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector2.up * (moveVertical * speed * Time.deltaTime));
+        transform.Translate(Vector2.right * (moveHorizontal * speed * Time.deltaTime));
+    }
+}
