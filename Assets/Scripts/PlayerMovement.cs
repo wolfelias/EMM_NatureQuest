79249ce7f9bdce042f70eedac5122fdc9bd2ae10
sunanmau5 <s0566144@htw-berlin.de>
2020-12-26 19:35:46 +0100

    updated player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,14 +6,18 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
     private int speed = 10;
+    public Rigidbody2D rb;
 
     // Update is called once per frame
     void Update()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        // float moveHorizontal = Input.GetAxis("Horizontal");
+        // float moveVertical = Input.GetAxis("Vertical");
 
-        transform.Translate(Vector2.up * (moveVertical * speed * Time.deltaTime));
-        transform.Translate(Vector2.right * (moveHorizontal * speed * Time.deltaTime));
+        // transform.Translate(Vector2.up * (moveVertical * speed * Time.deltaTime));
+        // transform.Translate(Vector2.right * (moveHorizontal * speed * Time.deltaTime));
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+        // transform.position = transform.position + movement * speed * Time.deltaTime;
+        rb.velocity = new Vector2(movement.x * speed, movement.y * speed);
     }
 }
