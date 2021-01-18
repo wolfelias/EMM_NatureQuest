c383fd2274ef7ab5b24a46d3ba33af6133fc6a96
MVVN <Minh.Vu@Student.HTW-Berlin.de>
2021-01-19 00:55:21 +0100

    fixed: player cant move (timeScale) and new Menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool IsPaused = false;
+    public GameObject pauseMenu;
+
+    private void Start()
+    {
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false);
+        IsPaused = false;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(IsPaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+        IsPaused = true;
+    }
+
+    public void Resume()    
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+        IsPaused = false;
+    }
+
+    public void LoadStartMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("StartMenu");
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
