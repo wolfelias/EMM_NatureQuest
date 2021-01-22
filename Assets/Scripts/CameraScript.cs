277907717d83cbd9139cb5dc2e4fdb8785f73ab4
MVVN <Minh.Vu@Student.HTW-Berlin.de>
2021-01-22 17:50:20 +0100

    added new cam package for cam follow & more options

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -5,23 +5,13 @@ using UnityEngine;
 public class CameraScript : MonoBehaviour
 {
     public GameObject player;
-    private Quaternion rotation;
     private Vector3 offsetRotated;
-
-    public int interpolationFramesCount; // Number of frames to completely interpolate between the 2 positions
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 offsetVector = new Vector3(0.0f, 1.0f, -30.5f);
         
-        transform.position = Vector3.Lerp(transform.position, player.transform.position + offsetVector,
-            0.15f);
+        transform.position = Vector3.Lerp(transform.position, player.transform.position + offsetVector, 0.15f);
     }
 }
