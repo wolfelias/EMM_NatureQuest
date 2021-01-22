1e9eab147080af29a0a77f39eb15e26287fcba1d
sunanmau5 <s0566144@htw-berlin.de>
2021-01-22 12:01:24 +0100

    added points for health

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class HealthBar : MonoBehaviour
 {
     public Slider healthBar;
     public Health playerHealth;
+    public TextMeshProUGUI mText;
 
     // Start is called before the first frame update
     private void Start()
@@ -15,10 +17,13 @@ public class HealthBar : MonoBehaviour
         healthBar = GetComponent<Slider>();
         healthBar.maxValue = playerHealth.maxHealth;
         healthBar.value = playerHealth.maxHealth / 2;
+        mText = GameObject.Find("Health Bar Points").GetComponent<TextMeshProUGUI>();
+        mText.SetText(healthBar.value +" / "+ playerHealth.maxHealth);
     }
 
     public void SetHealth(int hp)
     {
         healthBar.value = hp;
+        mText.SetText(hp +" / "+ playerHealth.maxHealth);
     }
 }
