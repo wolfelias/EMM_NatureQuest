2bd0c9ab965a4b59e2910cf896cbdf2e493e833a
sunanmau5 <s0566144@htw-berlin.de>
2021-01-22 15:28:20 +0100

    added floating points when health increases or decreases

---
+++ Assets/Scripts/FloatingTextHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingTextHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 2f);
+        transform.localPosition += new Vector3(0, 25, 0);
+    }
+}
