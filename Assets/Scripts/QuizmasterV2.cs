b97ec66eec12bb09c19e745525a3b3d05a89f3a3
MVVN <Minh.Vu@Student.HTW-Berlin.de>
2021-01-24 16:58:35 +0100

    prepared for merge

--- Assets/Scripts/QuizmasterV2.cs
+++ Assets/Scripts/QuizmasterV2.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Linq;
 
-public class Quizmaster : MonoBehaviour
+public class QuizmasterV2 : MonoBehaviour
 {
     private Health playerHealth;
     private int playerOnEnterHealth;
