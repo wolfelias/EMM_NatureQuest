36b987d27be57ad3775f0b41d3432a7972446f04
MVVN <Minh.Vu@Student.HTW-Berlin.de>
2021-01-25 12:50:47 +0100

    different answers to questions now possible

--- Assets/Scripts/Question.cs
+++ Assets/Scripts/Question.cs
@@ -7,6 +7,10 @@ public class Question
 {
     [TextArea(2, 7)]
     public string question;
+    [TextArea(2, 8)]
+    public string answerRight;
+    [TextArea(2, 8)]
+    public string answerWrong;
     public bool isTrue;
 
     public string getQuestion { get { return question; } }
