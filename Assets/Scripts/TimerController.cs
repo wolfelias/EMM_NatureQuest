c17f420b2f0e61ff576f3765082f682dbbf0676d
wolfelias <w.e.kaminski@gmail.com>
2021-01-25 23:46:04 +0100

    improved timer

--- Assets/Scripts/TimerController.cs
+++ Assets/Scripts/TimerController.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class TimerController : MonoBehaviour
@@ -21,6 +22,15 @@ public class TimerController : MonoBehaviour
     public TimeSpan maxTime;
 
     public double time;
+
+    public GameObject lostPanel;
+
+    public GameObject restartButton;
+
+    public GameObject mainMenuButton;
+
+    private bool isOver = false;
+
 
     private void Awake()
     {
@@ -48,6 +58,7 @@ public class TimerController : MonoBehaviour
         timerGoing = false;
     }
 
+
     private IEnumerator UpdateTimer()
     {
         while (timerGoing)
@@ -57,17 +68,27 @@ public class TimerController : MonoBehaviour
             string timePlayingStr = "Time: " + maxTime.ToString("mm':'ss'.'ff");
             timeCounter.text = timePlayingStr;
 
-            if (TimeSpan.Compare(maxTime, TimeSpan.FromSeconds(30)) == -1)
+            if (TimeSpan.Compare(maxTime, TimeSpan.FromSeconds(20)) == -1)
             {
                 timeCounter.color = new Color(255, 0, 0, 100);
             }
 
-            if (TimeSpan.Compare(maxTime, TimeSpan.FromSeconds(0)) == 0 ||
-                TimeSpan.Compare(maxTime, TimeSpan.FromSeconds(0)) == -1)
+            if ((TimeSpan.Compare(maxTime, TimeSpan.FromSeconds(0)) == 0 ||
+                 TimeSpan.Compare(maxTime, TimeSpan.FromSeconds(0)) == -1) && !isOver)
             {
                 Time.timeScale = 0;
                 timeCounter.text = "YOU LOST";
+                lostPanel.SetActive(true);
+
+                mainMenuButton.gameObject.SetActive(true);
+                restartButton.gameObject.SetActive(true);
+
+                EventSystem.current.SetSelectedGameObject(null);
+                EventSystem.current.SetSelectedGameObject(restartButton);
+                
+                isOver = true;
             }
+
             yield return null;
         }
     }
