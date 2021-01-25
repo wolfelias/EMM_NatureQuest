3d6af15d9f8a9ab4d85dfeacb7269704c8cd9194
wolfelias <w.e.kaminski@gmail.com>
2021-01-25 16:46:47 +0100

    bugfixes

--- Assets/Scripts/Maximise.cs
+++ Assets/Scripts/Maximise.cs
@@ -5,29 +5,29 @@ using UnityEngine;
 public class Maximise : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject gameObject;
+    public GameObject miniMap;
     public Camera camera;
     bool mapIsOpened = false;
 
-
-    void Start()
-    {
-       
-
+    void Start(){
+        miniMap.transform.localScale = new Vector3 (1f, 1f, 1f);
+        miniMap.transform.position = new Vector3(Screen.width-200, Screen.height-200);
+        camera.fieldOfView = 10f;
     }
+
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown("m")){
             if(mapIsOpened){
-                gameObject.transform.localScale = new Vector3 (1, 1f, 1);
-                gameObject.transform.position = new Vector3(Screen.width/1.15f, Screen.height/1.2f);
-                camera.fieldOfView = 16f;
+                miniMap.transform.localScale = new Vector3 (1f, 1f, 1f);
+                miniMap.transform.position = new Vector3(Screen.width-200, Screen.height-200);
+                camera.fieldOfView = 10f;
 
             }else{
-                gameObject.transform.localScale = new Vector3 (3, 3f, 3);
-                gameObject.transform.position = new Vector3(Screen.width/2, Screen.height/2);
+                miniMap.transform.localScale = new Vector3 (2f, 2f, 2f);
+                miniMap.transform.position = new Vector3(Screen.width/2, Screen.height/2);
                 camera.fieldOfView = 16f;
             }
             mapIsOpened = !mapIsOpened;
