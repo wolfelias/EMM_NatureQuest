b7c1e6b81f2c76f290c255abcacc95c195b75e3d
wolfelias <w.e.kaminski@gmail.com>
2021-01-25 15:33:38 +0100

    wip

---
+++ Assets/Scripts/CarAgentScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CarAgentScript : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Camera camera;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!(camera is null))
+            {
+                Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.collider.tag == "Ground")
+                    {
+                        Debug.Log("YEAAH");
+                        agent.SetDestination(hit.point);
+                    
+                    }
+                }
+            }
+        }
+    }
+}
