6b8c9028e4888526b3ac96a946c999069c434c9e
sunanmau5 <s0566144@htw-berlin.de>
2021-01-25 13:05:23 +0100

    update main menu to enable user to navigate using WASD

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class StartMenu : MonoBehaviour
 {
@@ -12,11 +13,57 @@ public class StartMenu : MonoBehaviour
     public Button survivalButton;
     public GameObject survivalText;
     public GameObject chillText;
+    public GameObject menuFirstButton, modeFirstButton, aboutFirstButton, aboutClosedButton; 
 
     private void Start()
     {
         survivalText.gameObject.SetActive(false);
         chillText.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (EventSystem.current.currentSelectedGameObject == survivalButton.gameObject)
+        {
+            ShowSurvivalInfo();
+            ExitChillInfo();
+        }
+        else if (EventSystem.current.currentSelectedGameObject == chillButton.gameObject)
+        {
+            ShowChillInfo();
+            ExitSurvivalInfo();
+        }
+        else
+        {
+            ExitSurvivalInfo();
+            ExitChillInfo();
+        }
+    }
+
+    public void OpenChooseMode()
+    {
+        // Clear selected object
+        EventSystem.current.SetSelectedGameObject(null);
+        // Set a new selected object
+        EventSystem.current.SetSelectedGameObject(modeFirstButton);
+    }
+
+    public void CloseChooseMode()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(menuFirstButton);
+    }
+
+    public void OpenAbout()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(aboutFirstButton);
+    }
+
+    public void CloseAbout()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(aboutClosedButton);
     }
 
     public void ShowSurvivalInfo()
